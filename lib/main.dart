55e0e2e534d408409d8d5d98771dd4b41ff9bc65
Joao Paulo <joao_paulo_oandrade@hotmail.com>
2025-05-06 22:18:56 -0300

    adicionando botao interativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,33 @@
+import 'dart:convert';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(home: Restaurante()));
 }
 
-class Restaurante extends StatelessWidget {
+class Restaurante extends StatefulWidget {
   const Restaurante({super.key});
+
+  @override
+  State<Restaurante> createState() => _RestauranteState();
+}
+
+class _RestauranteState extends State<Restaurante> {
+
+  int cont = 0;
+  void incrementar(){
+    setState(() {
+    cont++;
+    });
+
+  }
+  void descrementar(){
+    setState(() {
+    cont--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +38,17 @@ class Restaurante extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
         Text("Pode Entrar", style: TextStyle(fontSize: 30,color: Colors.white),),
-        Text("0", style: TextStyle(fontSize: 100,color: Colors.white),),
+        Text(cont.toString(), style: TextStyle(fontSize: 100,color: Colors.white),),
         Row(mainAxisAlignment: MainAxisAlignment.center,
           children: [
 
-          TextButton(onPressed: null,
+          TextButton(onPressed: incrementar,
             style: TextButton.styleFrom(backgroundColor: Colors.white, fixedSize: Size(100, 70), shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(24))), 
             child: Text("Entrar", style: TextStyle(fontSize: 20,color: Colors.black,)),),
           
           SizedBox(width: 20,),
 
-          TextButton(onPressed: null, 
+          TextButton(onPressed: descrementar, 
             style: TextButton.styleFrom(backgroundColor: Colors.white, fixedSize: Size(100, 70), shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(24))), 
             child: Text("Sair", style: TextStyle(fontSize: 20,color: Colors.black,)),),
           ],)
@@ -44,4 +66,6 @@ class Restaurante extends StatelessWidget {
     );
   }
 }
+
+
 
